7cbaca163441ad9d7899a1901b46ff0fd9c008de
N.I <iworksWeb@gmail.com>
2025-01-22 15:20:39 +0900

    add Todo list functionality to TRPC server
    - Introduced a new Todo interface and a sample todoList array.
    - Added a new TRPC route 'getTodoList' to retrieve the list of todos.
    - Updated server.ts to include the new route for accessing the todo list.

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,22 @@ import express from "express";
 import { initTRPC } from "@trpc/server";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
+interface Todo {
+  id: number;
+  content: string;
+}
+
+const todoList: Todo[] = [
+  {
+    id: 1,
+    content: "test",
+  },
+  {
+    id: 2,
+    content: "test2",
+  },
+];
+
 const app = express();
 const PORT = 5000;
 
@@ -14,11 +30,15 @@ const router = t.router; // ルーターを作成
 const publicProcedure = t.procedure; // パブリックプロシージャーを作成
 
 const appRouter = router({
+  // http://localhost:5000/trpc/test でアクセス可能
   test: publicProcedure.query(() => {
     return "Hello TRPC";
   }),
+  // http://localhost:5000/trpc/getTodoList でアクセス可能
+  getTodoList: publicProcedure.query(() => {
+    return todoList;
+  }),
 });
-// http://localhost:5000/trpc/test でアクセスできる
 app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 app.listen(PORT);
