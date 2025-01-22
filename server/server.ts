e67b5dedd6a2fde5f9fb713d2319da07bebd383b
N.I <iworksWeb@gmail.com>
2025-01-22 15:17:09 +0900

    Implement TRPC integration with Express server, adding a test route for querying "Hello TRPC".

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { initTRPC } from "@trpc/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
 const app = express();
 const PORT = 5000;
@@ -7,4 +9,19 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+const t = initTRPC.create(); // トランスポートレイヤーを作成
+const router = t.router; // ルーターを作成
+const publicProcedure = t.procedure; // パブリックプロシージャーを作成
+
+const appRouter = router({
+  test: publicProcedure.query(() => {
+    return "Hello TRPC";
+  }),
+});
+// http://localhost:5000/trpc/test でアクセスできる
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+
 app.listen(PORT);
+
+// publicProcedure.query(() => "Hello World"); // query : 値の取得 GET
+// publicProcedure.mutation(() => "Hello World"); // mutation : 値の操作 POST UPDATE DELETE
